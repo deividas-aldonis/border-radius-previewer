b9ffeaf78c186af75158271a48a62229ee10894c
Deividas <deividas.aldonis.dev@gmail.com>
2022-12-09 15:31:47 +0200

    Add functionality to change border radius of preview box based on inputs

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Hello!");
+const borderTopLeftInput = document.querySelector(".border-top-left-input");
+const borderTopRightInput = document.querySelector(".border-top-right-input");
+const borderBottomLeftInput = document.querySelector(
+  ".border-bottom-left-input"
+);
+const borderBottomRightInput = document.querySelector(
+  ".border-bottom-right-input"
+);
+
+const borderTopLeftSelect = document.querySelector(".border-top-left-select");
+const borderTopRightSelect = document.querySelector(".border-top-right-select");
+const borderBottomLeftSelect = document.querySelector(
+  ".border-bottom-left-select"
+);
+const borderBottomRightSelect = document.querySelector(
+  ".border-bottom-right-select"
+);
+
+const box = document.querySelector(".box");
+
+const changeBorderRadius = (e, property, unit) => {
+  const value = e.target.value || 0;
+  box.style[property] = value + unit;
+};
+
+borderTopLeftInput.addEventListener("input", (e) => {
+  changeBorderRadius(e, "borderTopLeftRadius", borderTopLeftSelect.value);
+});
+borderTopRightInput.addEventListener("input", (e) => {
+  changeBorderRadius(e, "borderTopRightRadius", borderTopRightSelect.value);
+});
+borderBottomLeftInput.addEventListener("input", (e) => {
+  changeBorderRadius(e, "borderBottomLeftRadius", borderBottomLeftSelect.value);
+});
+borderBottomRightInput.addEventListener("input", (e) => {
+  changeBorderRadius(
+    e,
+    "borderBottomRightRadius",
+    borderBottomRightSelect.value
+  );
+});
