58db6eec8c0c4660c3431f3092f1b6bbe8328c1a
Deividas <deividas.aldonis.dev@gmail.com>
2022-12-09 15:44:38 +0200

    Recalculate box border radius when switching units

--- script.js
+++ script.js
@@ -18,24 +18,64 @@ const borderBottomRightSelect = document.querySelector(
 
 const box = document.querySelector(".box");
 
-const changeBorderRadius = (e, property, unit) => {
-  const value = e.target.value || 0;
-  box.style[property] = value + unit;
+const changeBorderRadius = (value, property, unit) => {
+  box.style[property] = (value || 0) + unit;
 };
 
 borderTopLeftInput.addEventListener("input", (e) => {
-  changeBorderRadius(e, "borderTopLeftRadius", borderTopLeftSelect.value);
+  changeBorderRadius(
+    e.target.value,
+    "borderTopLeftRadius",
+    borderTopLeftSelect.value
+  );
 });
 borderTopRightInput.addEventListener("input", (e) => {
-  changeBorderRadius(e, "borderTopRightRadius", borderTopRightSelect.value);
+  changeBorderRadius(
+    e.target.value,
+    "borderTopRightRadius",
+    borderTopRightSelect.value
+  );
 });
 borderBottomLeftInput.addEventListener("input", (e) => {
-  changeBorderRadius(e, "borderBottomLeftRadius", borderBottomLeftSelect.value);
+  changeBorderRadius(
+    e.target.value,
+    "borderBottomLeftRadius",
+    borderBottomLeftSelect.value
+  );
 });
 borderBottomRightInput.addEventListener("input", (e) => {
   changeBorderRadius(
-    e,
+    e.target.value,
     "borderBottomRightRadius",
     borderBottomRightSelect.value
+  );
+});
+
+borderTopLeftSelect.addEventListener("change", (e) => {
+  changeBorderRadius(
+    borderTopLeftInput.value,
+    "borderTopLeftRadius",
+    e.target.value
+  );
+});
+borderTopRightSelect.addEventListener("change", (e) => {
+  changeBorderRadius(
+    borderTopRightInput.value,
+    "borderTopRightRadius",
+    e.target.value
+  );
+});
+borderBottomLeftSelect.addEventListener("change", (e) => {
+  changeBorderRadius(
+    borderBottomLeftInput.value,
+    "borderBottomLeftRadius",
+    e.target.value
+  );
+});
+borderBottomRightSelect.addEventListener("change", (e) => {
+  changeBorderRadius(
+    borderBottomRightInput.value,
+    "borderBottomRightRadius",
+    e.target.value
   );
 });
